6fa962121a3be159d88179fb6e5979f92a8fb9f3
Christine McGlade <cmcglade825@gmail.com>
2015-12-06 22:47:32 -0500

    prepping for wordpress part 3

--- wp-content/themes/child_twentythirteen/custom-page.php
+++ wp-content/themes/child_twentythirteen/custom-page.php
@@ -1,11 +1,7 @@
 	
 <?php
-/**
+/*
  * Template Name: My Custom Page
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
  */
 
 get_header(); ?>
@@ -17,6 +13,11 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					
+					<!--don't want to show title on front page-->
+					<?php
+					if ( !is_front_page() ) { ?>
+					
 					<header class="entry-header">
 						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
 						<div class="entry-thumbnail">
@@ -26,7 +27,9 @@ get_header(); ?>
 
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 					</header><!-- .entry-header -->
-
+					
+					<?php } ?>
+					
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
@@ -43,5 +46,6 @@ get_header(); ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<!--commenting out the sidebar -->
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
